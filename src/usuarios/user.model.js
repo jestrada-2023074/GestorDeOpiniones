b04236167f779e5708db07445742973a2bfa642e
Informatica <informatica@alumno.local>
2025-02-20 16:46:59 -0600

    Se crea el modelo de usuarios, se crea los package.json

---
+++ src/usuarios/user.model.js
@@ -0,0 +1,55 @@
+//Modelo de usuarios
+import { Schema, model} from 'mongoose'
+
+const userSchema = Schema(
+    {
+        name:{
+            type: String,
+            requerid: [true, 'Name is required'],
+            MaxLength: [30, `Can't be overcome 30 characters`]
+        },
+        surname: {
+            type: String,
+            required: [true, 'Surname is required'],
+            maxLength: [25, `Can't be overcome 25 characters`]
+        },
+        username: {
+            type: String,
+            required: [true, 'Username is required'],
+            unique: [true, 'Username is already taken'], 
+            lowercase: true,
+            maxLength: [15, `Can't be overcome 15 characters`]
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: [true, 'Email is required'],
+        },
+        password: {
+            type: String,
+            required: [true, 'Password is required'],
+            minLength: [8, 'Password must be 8 characters'],
+            maxLength: [100, `Can't be overcome 100 characters`],
+            
+        },
+        phone: {
+            type: String,
+            required: [true, 'Phone is required'],
+            minLength: [8, `Can't be overcome 16 characters`],
+            maxLength: [15, 'Phone must be 15 numbers'],
+        },
+        role: {
+            type: String,
+            required: [true, 'Role is required'],
+            uppercase: true,
+            enum: ['ADMIN', 'CLIENT']
+        }
+    }
+)
+
+userSchema.methods.toJSON = function(){
+    const { __v, password, ...user} = this.toObject() 
+    return user
+}
+
+export default model('User', userSchema)
